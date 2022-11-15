5e14d5edef6ea7a53d73f3bfe441d6c85b6387af
Guillermo Oscar <guillermobianco9090@gmail.com>
2022-11-15 01:10:03 -0300

    commit 002

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -41,7 +41,7 @@ function queryTestDb(query, config) {
   const connection = mysql.createConnection(config.env.db);
   // start connection to db
   connection.connect();
-  // exec query + disconnect to db as a Promise
+  //ejecutar consulta + desconectar a db con una promesa
   return new Promise((resolve, reject) => {
     connection.query(query, (error, results) => {
       if (error) reject(error);
