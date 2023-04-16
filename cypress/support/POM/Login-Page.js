216b3b443e1de14ad4ec633e6a620e6264e59117
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-16 01:41:22 -0300

    Archivo js pagina de login

---
+++ cypress/support/POM/Login-Page.js
@@ -0,0 +1,23 @@
+class Login {
+
+    get = {
+        userNameInput: () => cy.get('[name="username"]'),
+        passwordInput: () => cy.get('[name="password"]'),
+        submitButton: () => cy.get('[type="submit"]'),
+    }
+
+    // Acciones
+    enterUsername(type){
+        this.get.userNameInput().type(type)
+    }
+
+    enterPassword(type){
+        this.get.passwordInput().type(type)
+    }
+
+    submitLogin(){
+        this.get.submitButton().click()
+    }
+
+}
+
