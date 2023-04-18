f8d96bd77fe30771ff8f0b6ace7d62fb9d0247c9
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-18 00:27:30 -0300

    login.cy file

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -1,17 +1,21 @@
 import { login } from "../../support/POM/Login-Page";
 
-const loginPage = Cypress.env('endpoint').authLogin
-const 
+const {authLogin, dashboardIndex} = Cypress.env('endpoint')
+const {username, password} = Cypress.env('AdminUser')
 
 describe ('user logs in', () => {
 
     beforeEach(() => {
         cy.visit("/")
-        cy.url().should("contain", loginPage)
+        cy.url().should("contain", authLogin)
     });
 
     it('validate login successfully', () => {
+        login.enterUsername(username)
+        login.enterPassword(password)
+        login.submitLogin()
 
+        cy.url().should("contain", dashboardIndex)
     });
 
 });
